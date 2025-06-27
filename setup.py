f46e5e49653f329ab4f18dfbd66c298dfbdf7bce
Pierre Humblot-Ferrero <phf@grand-shooting.com>
2025-06-27 14:08:52 +0200

    UPDATED to version 1.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="notion-dev",
-    version="1.0.1",
+    version="1.0.2",
     author="Your Name",
     author_email="your.email@example.com",
     description="Integration tool for Notion, Asana and Git workflows",
